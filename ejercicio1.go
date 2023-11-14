2ef887750aa7e7ddbcb05c923be0183f82656764
juanmontilva <montilva.juancarlos@gmail.com>
2023-11-14 20:57:01 -0300

    actualizacion

--- ejercicio1.go
+++ ejercicio1.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-var num1 int
-
 func main() {
-	fmt.("ingresa un numero: ", num1)
+	var num1 int
+	// var num2 int
+	fmt.Println("ingresa un numero1: ")
+	fmt.Scan(&num1)
+	// fmt.Println("ingresa un numero2: ")
+	// fmt.Scan(&num2)
+
+	if num1 > 0 {
+		fmt.Println("es mayor a 0")
+	} else if num1 < 0 {
+		fmt.Println("es menor a 0")
+		// 	fmt.Println("num1 es mayor a num2")
+		// } else {
+		// 	fmt.Println("num2 es mayor a num1")
+	} else {
+		fmt.Println("es igual a 0")
+	}
 }
